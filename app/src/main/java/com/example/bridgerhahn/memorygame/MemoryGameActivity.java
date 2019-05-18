361fb7299827ee7bca070219d4096a282dcde7be
bridgar <bridgerhahn@gmail.com>
2019-05-17 20:49:49 -0700

    Switched game screens to fullscreen with no nav bar.

--- app/src/main/java/com/example/bridgerhahn/memorygame/MemoryGameActivity.java
+++ app/src/main/java/com/example/bridgerhahn/memorygame/MemoryGameActivity.java
@@ -2,9 +2,11 @@ package com.example.bridgerhahn.memorygame;
 
 import android.content.Intent;
 import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -20,7 +22,11 @@ public class MemoryGameActivity extends AppCompatActivity {
         int difficulty = intent.getIntExtra("difficulty", 0);
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
-        getSupportActionBar().hide(); //hide the title bar
+        try {
+            getSupportActionBar().hide(); //hide the title bar
+        } catch(NullPointerException e) {
+
+        }
 
         Display display = getWindowManager().getDefaultDisplay();
 
@@ -29,8 +35,15 @@ public class MemoryGameActivity extends AppCompatActivity {
 
         board = new MemoryGameBoard(this, difficulty, size);
 
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
+        View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
 
         setContentView(board);
     }
@@ -39,6 +52,15 @@ public class MemoryGameActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
         board.resume();
     }
 
--- app/src/main/java/com/example/bridgerhahn/memorygame/SnakeGameActivity.java
+++ app/src/main/java/com/example/bridgerhahn/memorygame/SnakeGameActivity.java
@@ -2,9 +2,11 @@ package com.example.bridgerhahn.memorygame;
 
 import android.content.Intent;
 import android.graphics.Point;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -19,7 +21,11 @@ public class SnakeGameActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
-        getSupportActionBar().hide(); //hide the title bar
+        try {
+            getSupportActionBar().hide(); //hide the title bar
+        } catch(NullPointerException e) {
+
+        }
 
         Display display = getWindowManager().getDefaultDisplay();
 
@@ -28,8 +34,15 @@ public class SnakeGameActivity extends AppCompatActivity {
 
         board = new SnakeGameBoard(this, size);
 
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
+        View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
 
         setContentView(board);
     }
@@ -38,6 +51,15 @@ public class SnakeGameActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
         board.resume();
     }
 
